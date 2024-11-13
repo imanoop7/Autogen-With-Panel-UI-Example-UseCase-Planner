23cdeda8f806487305a27d10b1c18e6ba2123b93
Anoop Maurya <64773373+imanoop7@users.noreply.github.com>
2024-11-13 23:15:22 +0530

    updated code

--- app_async.py
+++ app_async.py
@@ -1,8 +1,5 @@
 import autogen
 import panel as pn
-import openai
-import os
-import time
 import asyncio
 
 llm_config_list = [
@@ -56,8 +53,8 @@ engineer_agent = autogen.AssistantAgent(
     human_input_mode="NEVER",
     llm_config=llm_global_config,
     system_message='''Engineer. You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor.
-Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor.
-If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
+    Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor.
+    If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
 ''',
 )
 
@@ -74,8 +71,8 @@ planner_agent = autogen.AssistantAgent(
     name="Planner",
     human_input_mode="NEVER",
     system_message='''Planner. Suggest a plan. Revise the plan based on feedback from admin and critic, until admin approval.
-The plan may involve an engineer who can write code and a scientist who doesn't write code.
-Explain the plan first. Be clear which step is performed by an engineer, and which step is performed by a scientist.
+    The plan may involve an engineer who can write code and a scientist who doesn't write code.
+    Explain the plan first. Be clear which step is performed by an engineer, and which step is performed by a scientist.
 ''',
     llm_config=llm_global_config,
 )
